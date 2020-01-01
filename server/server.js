b232371ebc84b034bb4657d5682ff92e119b5303
unknown <gunjanbbps62@gmail.com>
2020-01-01 21:30:13 +0530

    add

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const express = require('express');
+
+var app=express();
+const publicPath = path.join(__dirname,'../public');
+app.use(express.static(publicPath));
+app.listen(3000,() => console.log("server is up"));
