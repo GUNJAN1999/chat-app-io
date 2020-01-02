a6d264b57777327df26c16826c622115c857ad9d
unknown <gunjanbbps62@gmail.com>
2020-01-02 14:09:24 +0530

    added new service

--- server/server.js
+++ server/server.js
@@ -1,7 +1,22 @@
 const path = require('path');
+const http=require('http')
 const express = require('express');
+const socketIO =require('socket.io');
 
-var app=express();
 const publicPath = path.join(__dirname,'../public');
+const port=process.env.PORT || 3000;
+var app=express();
+var server =http.createServer(app);
+var io=socketIO(server);
+
+io.on('connection',(socket)=> {
+	console.log('new user connected');
+
+	socket.on('disconnect',()=>{
+	console.log('user was disconnected');
+	});
+})
 app.use(express.static(publicPath));
-app.listen(3000,() => console.log("server is up"));
+
+
+server.listen(port,() => console.log(`Server is up on ${port}`));
