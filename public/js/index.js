2bf2aed63ffec6da21b42a91c3466961229c38ca
unknown <gunjanbbps62@gmail.com>
2020-01-03 06:40:18 +0530

    add form for meaasges and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,20 @@
 	console.log('disconnected from server');
 	});
 
-	socket.on('newMessage',(message)=>{
-		console.log('new message is recieved',message);
+	socket.on('newMessage',function (message){
+		console.log('new message ',message);
+		var li = jQuery('<li></li>');
+		li.text(`${message.from}: ${message.text}`);
+		jQuery('#messages').append(li);
+	});
+
+	jQuery("#message-form").on('submit',function (e) {
+		e.preventDefault();
+
+		socket.emit('createMessage',{
+			from:'user',
+			text  : jQuery('[name=message]').val()
+		},function (){
+
+		});
 	});
